f6095dd922e42d3b356c2016f9ee26298318cfdc
tubackit <52425822+tubackit@users.noreply.github.com>
2025-10-16 17:14:48 +0200

    ✅ GitHub Pages Konfiguration für tubackit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FoodFlash/', // Change this to your repository name
+  base: '/FoodFlash/', // GitHub Pages base path
 })
 
